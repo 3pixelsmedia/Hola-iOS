866a69eb40b97225e27386ecb9eb9234fc209170
cromanelli <christianromanelli@gmail.com>
2013-06-01 11:46:21 -0430

    Initial Commit

---
+++ Hello/HelloAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  HelloAppDelegate.h
+//  Hello
+//
+//  Created by Christian Romanelli on 01/06/13.
+//  Copyright (c) 2013 3 Pixels Media. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HelloAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Hello/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Hello
+//
+//  Created by Christian Romanelli on 01/06/13.
+//  Copyright (c) 2013 3 Pixels Media. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "HelloAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([HelloAppDelegate class]));
+    }
+}
